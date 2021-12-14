5c0e9abd0fcf493aa21943b3964b30d3a5f31168
Irfan6305 <94069792+Irfan6305@users.noreply.github.com>
2021-12-14 23:48:33 +0530

    Add files via upload

---
+++ ik khan.java
@@ -0,0 +1,42 @@
+//write pseudo code for printing the last 3digits of the given number 1557890
+print(1557890%1000)
+//write pseudocode for printing 1 number from beginning and 1 number from end for the given range [1-100]
+i=1
+while(i<=50)
+	print((i)+ " ",+(100-i+1))
+	i+=
+	//write pseudocode for printing the average of numbers from 12,13,14,15 using while loop
+	sum=0
+	i=12
+	while(i<=15) {
+		sum=sum+i
+				i=i+1}
+	}
+	//create a game that takes input as age and classifies the age as foolow
+	if(age<5)
+		print(baby)
+		if(age==10&&age<18)
+			print(teenaged)
+			if(age>18&&age==25)
+				print(adult)
+				if(age>25)
+					print(grown old)
+//check if the input number is prime or not
+					int x=n
+					i=2
+					while(i<n) {
+						if(x%i==0)
+							print(not prime)
+							else
+								print(prime num)
+					}
+	
+	//take square of sum up to 10 
+	sum=0
+			i=1
+			while(i<=10) {
+				sum=sum+i+i/i*i
+						i=i+1
+			}
+	print(sum)
+			}
